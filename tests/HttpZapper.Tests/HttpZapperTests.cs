ae90043a354562d193b05b323b634169ba9033d8
mruhul <mruhul@gmail.com>
2024-06-10 14:20:14 +1000

    Test for baseurl based call

--- tests/HttpZapper.Tests/HttpZapperTests.cs
+++ tests/HttpZapper.Tests/HttpZapperTests.cs
@@ -66,6 +66,56 @@ public class HttpZapperTests : IClassFixture<HttpZapperIocFixture>
                 Arg.Any<CancellationToken>());
         
     }
+    
+    [Fact]
+    public async Task Send_should_do_get_requests_with_correct_data_and_retrieve_correct_data_when_service_not_defined()
+    {
+        using var scope = _fixture.Scope();
+
+        var sut = scope.ServiceProvider.GetRequiredService<IHttpZapper>();
+
+        var serializer = scope.ServiceProvider.GetRequiredService<IHttpMessageSerializer>();
+        var httpClient = scope.ServiceProvider.GetRequiredService<IHttpClientWrapper>();
+        httpClient.ClearReceivedCalls();
+
+        HttpRequestMessage? givenRequest = null;
+        httpClient.Send(Arg.Any<string>(), Arg.Any<HttpRequestMessage>(),
+            Arg.Any<CancellationToken>())
+            .Returns(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(serializer.Serialize(new List<BookDto>
+                {
+                    new()
+                    {
+                        Id = "1",
+                        Isbn = "1sbn-1",
+                        Title = "title-1",
+                        Price = 23.99
+                    }
+                }))
+            }).AndDoes(x =>
+            {
+                givenRequest = x.Arg<HttpRequestMessage>();
+            });
+
+        var rsp = await sut.BaseUrl("http://api-books.prod.com.au")
+            .Path("/books")
+            .QueryString("test-qs","1")
+            .Header("test-h","1")
+            .Get<BookDto[]>(CancellationToken.None);
+
+        rsp.ShouldNotBeNull();
+        rsp.IsSuccessStatusCode.ShouldBeTrue();
+        rsp.StatusCode.ShouldBe(HttpStatusCode.OK);
+        givenRequest.ShouldNotBeNull();
+        givenRequest.RequestUri.ToString().ShouldBe("http://api-books.prod.com.au/books?test-qs=1");
+        givenRequest.Headers.ShouldContain(x => x.Key == "test-h" && string.Join(",",x.Value) == "1");
+        rsp.Content.ShouldContain(x => x.Id == "1");
+        await httpClient.Received(1).Send(Arg.Any<string>(), Arg.Any<HttpRequestMessage>(),
+                Arg.Any<CancellationToken>());
+        
+    }
 
     public record BookDto
     {
